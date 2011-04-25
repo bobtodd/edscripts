7c9c9441f136a5c00fbe86da4436d9264cebcfab
Todd Krause <toddbkrause@gmail.com>
2011-04-25 13:23:39 -0500

    File input handled, with optional header file

--- salary.py
+++ salary.py
@@ -3,7 +3,109 @@
 # salary.py
 # A script to arrange data in the form teacher-salary vs. student-grades
 
+import sys, os, csv
+import FileManip as fm
+
+def get_headers(infilename, headfilename=None):
+    """Get column names.
+
+    This function plucks out the names of columns in a CSV file
+    either from the file itself or from a separate, designated
+    header file.
+
+    Returns a tuple
+        (headers, ifile),
+    where headers is a list of column titles, and ifile is a
+    csv file object containing the input file.
+    """
+    
+    # open input file & header file (if different)
+    ifile           = csv.reader(open(  infilename, 'r'))  # open file for reading
+    if headfilename:
+        hbasename, hext = os.path.splitext(headfilename)
+    
+    # get the column headers
+    # from this file or a separate file
+    headers = []
+    if headfilename:
+        if hext == '.csv':
+            hfile    = csv.reader(open(headfilename, 'r'))  # open file for reading
+            for line in hfile:
+                headers += line                   # column headers may be spread over several lines
+        else:
+            hfile = open(headfilename, 'r')
+            for line in hfile:
+                headers += fm.extract_fields(line)
+            hfile.close()
+    else:
+        for line in ifile:
+            headers += line
+            break                             # should only have header in first line
+  
+    # get rid of leading and trailing whitespace (including newlines)
+    # in each column label
+    for i in range(len(headers)):
+        headers[i] = headers[i].strip()                # in case headers are split over multiple lines
+    if '' in headers:
+        headers.pop(headers.index(''))
+    
+    return headers, ifile
+
+
 # open files
+# default columns to be processed
+options = []
+headerfilename = None
+outfilename = 'output.txt'
+error = "Use --help option for assistance"
+
+# check for options
+if len(sys.argv) > 1:
+    if sys.argv[1] in ('-h', '-help', '--help'):
+        print("Usage: ", sys.argv[0], " [OPTIONS] headerfile datafile")
+        print("""
+  headerfile: The file from which you get the column names.
+  datafile:   The file containing the data to be processed.
+
+  Valid OPTIONS:
+    -h, -help, --help:
+              Evidently you've already discovered this one
+
+              """)
+        sys.exit(1)
+
+# read options
+while len(sys.argv) > 2:
+    option = sys.argv[1]
+    del sys.argv[1]
+    if option in ('-hd', '-head', '--header'):
+        headerfilename = sys.argv[1]
+        del sys.argv[1]
+    elif option in ('-o', '-oth', '--other'):
+        options.append(sys.argv[1])
+        del sys.argv[1]
+    else:
+        print(sys.argv[0] + ":",'invalid option', option)
+        print(error)
+        sys.exit(1)
+
+# get file names
+try:
+    datafilename = sys.argv[1]
+except:
+    print("Usage:", sys.argv[0], "[options] headerfile datafile")
+    print(error)
+    sys.exit(1)
+
+# open input file
+headers, ifile = get_headers(datafilename, headerfilename)
+
+print(headers)
+
+for line in ifile:
+    print(line)
+    break
+
 
 # get school codes, throw them all into a list or dict
 # perhaps best to just iterate through the teacher file
